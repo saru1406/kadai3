44fd2de8755f1d7714f13914590df64d674fa5bc
EC2 Default User <ec2-user@ip-172-31-8-59.ap-northeast-1.compute.internal>
2022-09-17 07:08:28 +0000

    update5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,11 +13,13 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def show
     @book = Book.find(params[:id])
-    
+    @user = current_user
+
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   get '/home/about'=>'homes#about',as:'about'
   patch 'books/:id'=>'books#update', as: 'update_book'
+  
   resources :books,only:[:new,:index,:show,:create,:destroy,:edit,]
-  resources:users,only:[:edit,:show,:index]
+  resources :users,only:[:edit,:show,:index,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
